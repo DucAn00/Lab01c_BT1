46f33264d69618b4a4c5214916c4a8dcc0c4ac53
DucAn00 <tranducan206dark@gmail.com>
2025-09-21 10:32:24 +0700

    Add project files.

---
+++ Controllers/th03Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01c_BT1.Controllers
+{
+    public class th03Controller : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Lab01c_BT1()
+        {
+            ViewBag.Message = "Your Lab01c_BT1 page.";
+            return View();
+        }
+        public ActionResult repass()
+        {
+            ViewBag.Message = "Your repass page.";
+            return View();
+        }
+        public ActionResult regsit()
+        {
+                       ViewBag.Message = "Your regsit page.";
+            return View();
+        }
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lab01c_BT1.MvcApplication" Language="C#" %>
